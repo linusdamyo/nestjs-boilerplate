fa61a877d7c9a4e40955b4e8da8ecf0db6257195
Geunsu Ryu <linusdamyo@gmail.com>
2025-03-17 18:37:48 +0900

    path @src, @test

--- test/app.e2e-spec.ts
+++ test/app.e2e-spec.ts
@@ -2,7 +2,7 @@ import { Test, TestingModule } from '@nestjs/testing';
 import { INestApplication } from '@nestjs/common';
 import request from 'supertest';
 import { App } from 'supertest/types';
-import { AppModule } from './../src/app.module';
+import { AppModule } from '@src/app.module';
 
 describe('AppController (e2e)', () => {
     let app: INestApplication<App>;
