cecf3b38529e209a6d68eac9203a6d06f0a5a55a
Geunsu Ryu <linusdamyo@gmail.com>
2025-03-17 18:28:29 +0900

    vitest

---
+++ vitest.config.ts
@@ -0,0 +1,23 @@
+import swc from 'unplugin-swc';
+import { resolve } from 'node:path';
+import { defineConfig } from 'vitest/config';
+
+export default defineConfig({
+    test: {
+        globals: true,
+        root: './',
+    },
+    plugins: [
+        // This is required to build the test files with SWC
+        swc.vite({
+            // Explicitly set the module type to avoid inheriting this value from a `.swcrc` config file
+            module: { type: 'es6' },
+        }),
+    ],
+    resolve: {
+        alias: {
+            // Ensure Vitest correctly resolves TypeScript path aliases
+            src: resolve(__dirname, './src'),
+        },
+    },
+});
